8fc1914ebc4b20bdb1973e9b43a94a7dc2fc61b2
Illia Holovatenko <illia.holovatenko@swanlogic.com>
2020-05-28 14:00:54 +0300

    Added ability to remove leading zero on input event.

--- lib/Number/index.jsx
+++ lib/Number/index.jsx
@@ -14,8 +14,11 @@ class NumberInput extends React.Component {
 	handleChange = (e) => {
 		this.props.onChange(+e.target.value);
 	}
+	handleInput = (e) => {
+		e.target.value = + e.target.value;
+	}
 	render() {
-		return <input {...this.props} min="0" onChange={this.handleChange} value={this.props.value} />;
+		return <input {...this.props} min="0" onInput={this.handleInput} onChange={this.handleChange} value={this.props.value} />;
 	}
 }
 
