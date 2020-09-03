0234cc62dda0c0d7e744a935bac201b848e00e24
Dmitriy Boychenko <marchiani22@gmail.com>
2020-09-03 17:46:41 +0300

    Comments were handled

--- lib/Date/index.jsx
+++ lib/Date/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Calendar from 'react-calendar/dist/entry.nostyle';
 
-import { withClassName } from 'react-deepgarden';
+import { withClassName, DropDown } from 'react-deepgarden';
 
 import Text from '../Text';
 
 import input from '../input';
-
-import CalendarDropDown from './CalendarDropDown';
 
 export default
 @withClassName('_Input')
@@ -23,44 +22,36 @@ class DateInput extends React.Component {
 	state = {
 		isShowCalendar: false,
 	};
-	handleInputChange = () => {
-	};
 	handleCalendarChange = (date) => {
 		!date.getHours() && date.setHours(Math.abs(date.getTimezoneOffset() / 60));
 		this.props.onChange(date.toLocaleDateString(this.props.locale, this.props.options));
-		this.hideCalendar();
+		this.toggleCalendar();
 	};
-	showCalendar = (e) => {
+	toggleCalendar = () => {
 		this.setState({
-			isShowCalendar: e.target,
+			isShowCalendar: !this.state.isShowCalendar,
 		});
-	};
-	hideCalendar = () => {
-		this.setState({
-			isShowCalendar: false,
-		});
-	};
+	}
 	render() {
 		const date = this.props.value ? new Date(this.props.value) : new Date();
 		return (
 			<>
 				<Text
 					value={this.props.value ? date.toLocaleDateString() : ''}
-					onChange={this.handleInputChange}
-					onClick={(e) => this.showCalendar(e)}
+					onClick={this.toggleCalendar}
 					disabled={this.props.disabled}
 				/>
 				{this.state.isShowCalendar
 				&& (
-					<CalendarDropDown
-						maxDate={this.props.maxDate}
-						minDate={this.props.minDate}
-						calendarType={this.props.calendarType}
-						coordinats={this.state.isShowCalendar.getBoundingClientRect()}
-						value={date}
-						handleCalendarChange={this.handleCalendarChange}
-						hideCalendar={this.hideCalendar}
-					/>
+					<DropDown direction="top" onClose={this.toggleCalendar} marginTop={-20} marginLeft={100}>
+						<Calendar
+							maxDate={this.props.maxDate}
+							minDate={this.props.minDate}
+							calendarType={this.props.calendarType}
+							value={date}
+							onChange={this.handleCalendarChange}
+						/>
+					</DropDown>
 				)}
 			</>
 		);
