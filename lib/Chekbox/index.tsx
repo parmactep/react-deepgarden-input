4c6bea28b4d12bf29423cb77e4a5ae230d2bf224
Par Mactep <parmactep@gmail.com>
2023-07-21 19:18:27 +0300

    Checkbox functional

--- lib/Chekbox/index.tsx
+++ lib/Chekbox/index.tsx
@@ -11,28 +11,25 @@ interface ICheckboxInputProps {
 	value?: boolean;
 }
 
-class CheckboxInput extends React.Component<ICheckboxInputProps> {
-	static defaultProps = {
-		value: false,
+function CheckboxInput({
+	onChange,
+	disabled,
+	value = false,
+}) {
+	const handleChange = (e: any) => {
+		onChange(e.target.checked);
 	};
-
-	handleChange = (e: any) => {
-		this.props.onChange(e.target.checked);
-	};
-
-	render() {
-		const value = !!+this.props.value;
-		return (
-			<div className={classNames('_CheckboxInput__Input', { '_CheckboxInput__Input--Checked': value }, { '_CheckboxInput--Disabled': this.props.disabled })}>
-				{value && (
-					<svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-						<path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
-					</svg>
-				)}
-				<input type="checkbox" disabled={this.props.disabled} onChange={this.handleChange} checked={value} />
-			</div>
-		);
-	}
+	const formattedValue = !!+value;
+	return (
+		<div className={classNames('_CheckboxInput__Input', { '_CheckboxInput__Input--Checked': formattedValue }, { '_CheckboxInput--Disabled': disabled })}>
+			{formattedValue && (
+				<svg stroke="currentColor" fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+					<path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
+				</svg>
+			)}
+			<input type="checkbox" disabled={disabled} onChange={handleChange} checked={formattedValue} />
+		</div>
+	);
 }
 
 import './index.styl';
